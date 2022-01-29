3c835616b5f393dd35f3b9bb0fcdc5f8bdc71d73
oanzhi <belan.angel.2000@gmail.com>
2022-01-29 02:58:58 +0200

    Initial commit

---
+++ src/test/java/com/socio/SocionicsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.socio;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SocionicsApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
